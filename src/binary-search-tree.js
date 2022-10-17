bd757b81d78f8dcfdea2a0afa737c33089295908
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2022-10-18 01:35:38 +0300

    feat: add root(),add() method to class BinarySearchTree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,39 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
 class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.treeRoot = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    return this.treeRoot;
+  }
+
+  add( data ) {
+
+    this.treeRoot = createNode( this.treeRoot, data);
+
+    function createNode( currentNode, data ) {
+
+      if ( !currentNode ) return new Node( data );
+      if ( currentNode.data === data) return currentNode;
+      if (data < currentNode.data) {
+        currentNode.left = insertNode( currentNode.left, data )
+      } else {
+        currentNode.right = insertNode(currentNode.right, data)
+      }
+      return currentNode;
+
+    }
+
   }
 
   has(/* data */) {
